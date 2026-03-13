5ea3eb57c7d0dcf2fd13e1bbda5a2ac852d7de11
roidsroll <roi398388@gmail.com>
2026-03-13 12:48:58 +0700

    `Kim `

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
+// next.config.ts
 import type { NextConfig } from "next";
+import PWA from "@ducanh2912/next-pwa";
+
+const withPWA = PWA({
+  dest: "public",
+  disable: false, // Set to false to see it in npm run dev
+  register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  workboxOptions: {
+    skipWaiting: true,
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "google-fonts",
+          expiration: { maxEntries: 4, maxAgeSeconds: 365 * 24 * 60 * 60 },
+        },
+      },
+      {
+        urlPattern: /\/api\/.*/i,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "api-cache",
+          networkTimeoutSeconds: 10,
+        },
+      },
+      {
+        urlPattern: /.*/i,
+        handler: "StaleWhileRevalidate",
+        options: { cacheName: "pages-cache" },
+      },
+    ],
+  },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
   images: {
     remotePatterns: [
       {
@@ -12,4 +48,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
